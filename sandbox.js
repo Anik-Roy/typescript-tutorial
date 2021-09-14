e56f8697357b813564c3e686cda614cbef54d4b3
Anik Roy <anik96lu@gmail.com>
2021-09-14 16:19:49 +0600

    variable declaration, mixed types, union types

---
+++ sandbox.js
@@ -0,0 +1,23 @@
+// let character: string;
+// let age: number;
+// let isLoggedIn: boolean;
+// let names: string[] = [];
+// names.push('anik');
+// console.log(names);
+// union types
+// let mixed: (string|number|boolean)[] = [];
+// mixed.push(20);
+// mixed.push('anik');
+// mixed.push(true);
+// console.log(mixed);
+// let my_obj: object;
+// my_obj = {name: 'anik', age: 24};
+// my_obj = {job: 'software engineer'};
+// console.log(my_obj);
+var my_obj2;
+my_obj2 = {
+    name: 'anik',
+    age: 24,
+    job: 'software engineer'
+};
+console.log(my_obj2);
